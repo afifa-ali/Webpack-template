9ce8e683510bd4f019f477f94fba12e73eb37bdd
afifa-ali <afifaali973@gmail.com>
2025-08-11 16:57:57 +0700

    prettier installed

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,7 +1,24 @@
-import js from "@eslint/js";
-import globals from "globals";
-import { defineConfig } from "eslint/config";
+import js from '@eslint/js'
+import globals from 'globals'
+import { defineConfig } from 'eslint/config'
 
 export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: {...globals.browser, ...globals.node} } },
-]);
+  {
+    files: ['**/*.{js,mjs,cjs}'],
+    plugins: { js },
+    extends: ['js/recommended'],
+    languageOptions: { globals: { ...globals.browser, ...globals.node } },
+  },
+])
+
+module.exports = {
+  extends: [
+    'eslint:recommended',
+    'plugin:prettier/recommended'
+  ],
+  plugins: ['prettier'],
+  rules: {
+    'prettier/prettier': 'error'
+  }
+};
+
--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,5 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
   entry: './src/index.js',
@@ -7,22 +7,22 @@ module.exports = {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.[contenthash].js',
     clean: true,
-    assetModuleFilename: 'assets/[name][ext]' // Optional: keeps original names
+    assetModuleFilename: 'assets/[name][ext]', // Optional: keeps original names
   },
   // mode: 'development', ← leave this out if you're setting it via CLI
   module: {
     rules: [
       {
         test: /\.html$/,
-        use: ['html-loader']
+        use: ['html-loader'],
       },
       {
         test: /\.css$/,
-        use: ['style-loader', 'css-loader']
+        use: ['style-loader', 'css-loader'],
       },
       {
         test: /\.(png|jpe?g|gif|svg)$/i,
-        type: 'asset/resource'
+        type: 'asset/resource',
       },
       {
         test: /\.js$/,
@@ -30,32 +30,30 @@ module.exports = {
         use: {
           loader: 'babel-loader',
           options: {
-            presets: ['@babel/preset-env']
-          }
-        }
-      }
-    ]
+            presets: ['@babel/preset-env'],
+          },
+        },
+      },
+    ],
   },
   plugins: [
     new HtmlWebpackPlugin({
       template: './src/index.html',
-      filename: 'index.html'
-    })
+      filename: 'index.html',
+    }),
   ],
   devServer: {
     static: {
-      directory: path.join(__dirname, 'dist')
+      directory: path.join(__dirname, 'dist'),
     },
     port: 8080,
     open: true,
     hot: true,
-    compress: true
+    compress: true,
   },
-  devtool: 'source-map'
-};
-
+  devtool: 'source-map',
+}
 
 /* npm install --save-dev webpack webpack-cli webpack-dev-server \
 html-webpack-plugin html-loader css-loader style-loader \
 @babel/core @babel/preset-env babel-loader */
-
